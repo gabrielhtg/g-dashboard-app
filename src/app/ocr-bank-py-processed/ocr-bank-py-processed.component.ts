3ec1229b6c07639a7d43c20a8e4d12503ca9875f
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-10-03 14:21:25 +0700

    menambahkan analitics

--- src/app/ocr-bank-py-processed/ocr-bank-py-processed.component.ts
+++ src/app/ocr-bank-py-processed/ocr-bank-py-processed.component.ts
@@ -1,102 +1,63 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 import {NgForOf} from "@angular/common";
-import {
-  ChartComponent,
-  ApexAxisChartSeries,
-  ApexChart,
-  ApexXAxis,
-  ApexDataLabels,
-  ApexTitleSubtitle,
-  ApexStroke,
-  ApexGrid
-} from "ng-apexcharts";
+import {BaseChartDirective} from "ng2-charts";
+import {ChartOptions, ChartData, ChartDataset, ChartConfiguration} from "chart.js";
+import {getDateFrequency, getMonthlyChartDebitData, getMonthlyChartLabels} from "./ocr-bank-py-processed.service";
 
-export type ChartOptions = {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  xaxis: ApexXAxis;
-  dataLabels: ApexDataLabels;
-  grid: ApexGrid;
-  stroke: ApexStroke;
-  title: ApexTitleSubtitle;
-};
 
 @Component({
   selector: 'app-ocr-bank-py-processed',
   standalone: true,
   imports: [
     NgForOf,
-    ChartComponent
+    BaseChartDirective,
   ],
   templateUrl: './ocr-bank-py-processed.component.html'
 })
-export class OcrBankPyProcessedComponent implements OnInit, AfterViewInit {
+export class OcrBankPyProcessedComponent implements OnInit{
   transactionData: any
-  analiticsData:any
-  @ViewChild("chart") chart: ChartComponent | undefined;
-  public chartOptions: Partial<ChartOptions> | undefined;
+  analyticsData:any
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  lineChartOptions: ChartConfiguration<"bar" | "line", number[], string | number>["options"] = {
+    responsive: true
+  }
+
+  // Data untuk debit kredit pertanggal yang ada pada bank statement
+  barChartDebitKreditLabels: (string | number)[] | undefined;
+  barChartDebitKreditData: ChartData<'bar', number[], string | number> | undefined
+
+  // Data untuk perbandingan debit dengan kredit (bar chart)
+  barTotalDebitKreditLabels: (string | number)[] | undefined;
+  barTotalDebitKreditData: ChartData<'bar', number[], string | number> | undefined
+
+  // Data untuk perbandingan banyaknya transaksi per tanggal yang ada pada bank statement
+  dateTransactionData : any
 
   constructor(private router: Router) {
     const navigation = this.router.getCurrentNavigation();
     this.transactionData = navigation?.extras.state?.['transaction-data'];
-    this.analiticsData = navigation?.extras.state?.['analitics-data'];
-  }
-
-  ngAfterViewInit(): void {
-    this.chartOptions = {
-      series: [
-        {
-          name: "DEBIT",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-        },
-        {
-          name: "CREDIT",
-          data: [20, 61, 55, 71, 69, 82, 89, 111, 168]
-        }
-      ],
-      chart: {
-        height: 350,
-        type: "line",
-        zoom: {
-          enabled: false
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        curve: "straight"
-      },
-      title: {
-        text: "Product Trends by Month",
-        align: "left"
-      },
-      grid: {
-        row: {
-          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-          opacity: 0.5
-        }
-      },
-      xaxis: {
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec"
-        ]
-      }
-    }
+    this.analyticsData = navigation?.extras.state?.['analitics-data'];
   }
 
   ngOnInit(): void {
+    this.barChartDebitKreditLabels = getMonthlyChartLabels(this.transactionData)
+    this.barChartDebitKreditData = {
+      labels: this.barChartDebitKreditLabels,
+      datasets: getMonthlyChartDebitData(this.transactionData)
+    };
+
+    this.barTotalDebitKreditLabels = ['Total Debit Kredit']
+    this.barTotalDebitKreditData = {
+      labels : this.barTotalDebitKreditLabels,
+      datasets : [
+        {data: [parseFloat(this.analyticsData.sum_cr.replaceAll(',', ''))], label: 'Kredit'},
+        {data: [parseFloat(this.analyticsData.sum_db.replaceAll(',', ''))], label: 'Debit'}
+      ]
+    }
+
+    this.dateTransactionData = getDateFrequency(this.transactionData)
   }
 }
